72322d3c0c3a7f52d1103d9c95a734545f0bb1ea
Nguyen Xuan Doan <112373423+nguyenxuandoan99@users.noreply.github.com>
2022-10-03 08:38:37 +0700

    d

--- my-app/src/app/shared/vote-button/vote-button.component.ts
+++ my-app/src/app/shared/vote-button/vote-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { throwError } from 'rxjs';
 import { AuthService } from 'src/app/auth/shared/auth.service';
 import { PostModel } from '../post-model';
 import { PostService } from '../post.service';
@@ -15,7 +16,7 @@ import { VoteType } from './vote-type';
 export class VoteButtonComponent implements OnInit {
 
   @Input() post!: PostModel;
-  public votePayload : any;
+  public votePayload !: VotePayload;
   faArrowUp = faArrowUp;
   faArrowDown = faArrowDown;
   upvoteColor !: string;
@@ -24,18 +25,37 @@ export class VoteButtonComponent implements OnInit {
 
   constructor(private voteService : VoteService, private authService : AuthService, private postService : PostService) {
     // this.votePayload = {
-    //   voteType: null,
+    //   voteType : undefined,
     //   postId : null
     // }
     // this.authService.loggedIn.subscribe((data : boolean) => this.isLoggedIn = data);
    }
 
   ngOnInit(): void {
-    // this.updateVoteDetails();
+    this.updateVoteDetails();
   }
 
   upvotePost():void{
 
   }
-  downvotePost():void{}
+  downvotePost():void{
+    this.votePayload.voteType = VoteType.DOWNVOTE;
+    this.vote();
+    this.upvoteColor = '';
+  }
+
+  private vote() {
+    this.votePayload.postId = this.post.id;
+    this.voteService.vote(this.votePayload).subscribe(() => {
+      this.updateVoteDetails();
+    }, error => {
+      // this.toastr.error(error.error.message);
+      throwError(error);
+    });
+  }
+  private updateVoteDetails() {
+    this.postService.getPost(this.post.id).subscribe(post => {
+      this.post = post;
+    });
+  }
 }
